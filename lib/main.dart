9b22233cfd3b753961f004f0563ee090cfb559ca
Edisoon <edison_theM3ka@hotmail.com>
2024-04-29 23:10:08 -0400

    First change

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
